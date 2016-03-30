70db0f736c95b4f15c62255fbe3bbb4df4fc99df
brianl <brian.londregan@gmail.com>
2016-03-29 22:33:41 -0400

    adding game selector

--- baseball/static/playerstatlist15.js
+++ baseball/static/playerstatlist15.js
@@ -1,6 +1,6 @@
 [
-{ "ID" : 1, "NAME" : "Wilmer Flores", "POS" : "SS", "STATS" : "59 RBIs | 55 Runs | 16 HR | .263 BA | .295 OBP | .408 SLG", "IMG" : "http://d3k2oh6evki4b7.cloudfront.net/req/201602120/images/headshots/e/effa9435_mlbam.jpg"},
-{ "ID" : 2, "NAME" : "Lucas Duda", "POS" : "1B", "STATS" : "73 RBIs | 67 Runs | 27 HR | .244 BA | .352 OBP | .486 SLG", "IMG" : "http://d3k2oh6evki4b7.cloudfront.net/req/201602120/images/headshots/b/bcd38711_mlbam.jpg"},
-{ "ID" : 3, "NAME" : "Curtis Granderson", "POS" : "LF", "STATS" : "70 RBIs | 98 Runs | 26 HR | .259 BA | .364 OBP | .457 SLG", "IMG" : "http://d3k2oh6evki4b7.cloudfront.net/req/201602120/images/headshots/e/e6dd937a_mlbam.jpg"},
-{ "ID" : 4, "NAME" : "Daniel Murphy", "POS" : "2B", "STATS" : "73 RBIs | 56 Runs | 14 HR | .281 BA | .322 OBP | .449 SLG", "IMG" : "http://d3k2oh6evki4b7.cloudfront.net/req/201602120/images/headshots/1/17225395_mlbam.jpg"}
+{ "ID" : 1, "NAME" : "Wilmer Flores", "POS" : "SS", "STATS" : "59 RBIs | 55 Runs | 16 HR | .263 BA | .295 OBP | .408 SLG", "IMG" : "http://d3k2oh6evki4b7.cloudfront.net/req/201602120/images/headshots/e/effa9435_mlbam.jpg", "GAMENO" : "1"},
+{ "ID" : 2, "NAME" : "Lucas Duda", "POS" : "1B", "STATS" : "73 RBIs | 67 Runs | 27 HR | .244 BA | .352 OBP | .486 SLG", "IMG" : "http://d3k2oh6evki4b7.cloudfront.net/req/201602120/images/headshots/b/bcd38711_mlbam.jpg", "GAMENO" : "1"},
+{ "ID" : 3, "NAME" : "Curtis Granderson", "POS" : "LF", "STATS" : "70 RBIs | 98 Runs | 26 HR | .259 BA | .364 OBP | .457 SLG", "IMG" : "http://d3k2oh6evki4b7.cloudfront.net/req/201602120/images/headshots/e/e6dd937a_mlbam.jpg", "GAMENO" : "1"},
+{ "ID" : 4, "NAME" : "Daniel Murphy", "POS" : "2B", "STATS" : "73 RBIs | 56 Runs | 14 HR | .281 BA | .322 OBP | .449 SLG", "IMG" : "http://d3k2oh6evki4b7.cloudfront.net/req/201602120/images/headshots/1/17225395_mlbam.jpg", "GAMENO" : "150"}
 ]
