a88b30443ab510d387c4f64da9a78e8573cedc0d
brianl <brian.londregan@gmail.com>
2016-03-29 23:03:12 -0400

    adding game selector

--- baseball/static/playerstatlist15.js
+++ baseball/static/playerstatlist15.js
@@ -2,5 +2,9 @@
 { "ID" : 1, "NAME" : "Wilmer Flores", "POS" : "SS", "STATS" : "59 RBIs | 55 Runs | 16 HR | .263 BA | .295 OBP | .408 SLG", "IMG" : "http://d3k2oh6evki4b7.cloudfront.net/req/201602120/images/headshots/e/effa9435_mlbam.jpg", "GAMENO" : "1"},
 { "ID" : 2, "NAME" : "Lucas Duda", "POS" : "1B", "STATS" : "73 RBIs | 67 Runs | 27 HR | .244 BA | .352 OBP | .486 SLG", "IMG" : "http://d3k2oh6evki4b7.cloudfront.net/req/201602120/images/headshots/b/bcd38711_mlbam.jpg", "GAMENO" : "1"},
 { "ID" : 3, "NAME" : "Curtis Granderson", "POS" : "LF", "STATS" : "70 RBIs | 98 Runs | 26 HR | .259 BA | .364 OBP | .457 SLG", "IMG" : "http://d3k2oh6evki4b7.cloudfront.net/req/201602120/images/headshots/e/e6dd937a_mlbam.jpg", "GAMENO" : "1"},
-{ "ID" : 4, "NAME" : "Daniel Murphy", "POS" : "2B", "STATS" : "73 RBIs | 56 Runs | 14 HR | .281 BA | .322 OBP | .449 SLG", "IMG" : "http://d3k2oh6evki4b7.cloudfront.net/req/201602120/images/headshots/1/17225395_mlbam.jpg", "GAMENO" : "150"}
+{ "ID" : 4, "NAME" : "Daniel Murphy", "POS" : "2B", "STATS" : "73 RBIs | 56 Runs | 14 HR | .281 BA | .322 OBP | .449 SLG", "IMG" : "http://d3k2oh6evki4b7.cloudfront.net/req/201602120/images/headshots/1/17225395_mlbam.jpg", "GAMENO" : "1"},
+{ "ID" : 5, "NAME" : "Juan Lagares", "POS" : "CF", "STATS" : "41 RBIs | 47 Runs | 6 HR | .259 BA | .289 OBP | .358 SLG", "IMG" : "http://d3k2oh6evki4b7.cloudfront.net/req/201602120/images/headshots/1/17225395_mlbam.jpg", "GAMENO" : "1"},
+{ "ID" : 6, "NAME" : "Yoenis Cespedes", "POS" : "OF", "STATS" : "44 RBIs | 39 Runs | 17 HR | .287 BA | .337 OBP | .604 SLG", "IMG" : "http://d3k2oh6evki4b7.cloudfront.net/req/201602120/images/headshots/1/17225395_mlbam.jpg", "GAMENO" : "103"},
+{ "ID" : 7, "NAME" : "John Mayberry", "POS" : "OF", "STATS" : "9 RBIs | 8 Runs | 3 HR | .164 BA | .227 OBP | .318 SLG", "IMG" : "http://d3k2oh6evki4b7.cloudfront.net/req/201602120/images/headshots/1/17225395_mlbam.jpg", "GAMENO" : "90"},
+{ "ID" : 8, "NAME" : "Eric Campbell", "POS" : "3B", "STATS" : "19 RBIs | 28 Runs | 3 HR | .197 BA | .312 OBP | .295 SLG", "IMG" : "http://d3k2oh6evki4b7.cloudfront.net/req/201602120/images/headshots/1/17225395_mlbam.jpg", "GAMENO" : "90"}
 ]
