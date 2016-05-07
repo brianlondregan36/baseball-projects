fbb8eadef62cc2f6743855d3440842037551cda4
brianl <brian.londregan@gmail.com>
2016-05-07 11:19:48 -0400

    still trying

--- baseball/views.py
+++ baseball/views.py
@@ -5,6 +5,7 @@ import logging
 logging.basicConfig(filename='log_filename.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.debug('This is a log message.')
 
+print("hello")
 @app.route('/')
 def index():
 	return render_template("index.html")
