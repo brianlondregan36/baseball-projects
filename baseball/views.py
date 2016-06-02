b1a5b3de4a8b71006eb59038c190733ed4f86c05
brianlondregan36 <brian.londregan@gmail.com>
2016-06-01 23:52:34 -0400

    fix for strange indent error

--- baseball/views.py
+++ baseball/views.py
@@ -9,10 +9,10 @@ queue = Queue.Queue()
 teams = {}
 
 class ThreadUrl(threading.Thread):
-	def __init__(self, queue, year):
-		threading.Thread.__init__(self)
-		self.queue = queue
-		self.year = year
+    def __init__(self, queue, year):
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.year = year
         
     def run(self):
         while True:
@@ -25,30 +25,30 @@ class ThreadUrl(threading.Thread):
 
 @app.route('/')
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
 
 @app.route('/lineupbuilder/')
 def builder():
-	return render_template("lineupbuilder.html")
-	
+    return render_template("lineupbuilder.html")
+    
 @app.route('/runsscoredgauntlet/')
 def gauntletpage():
-	return render_template("runsscoredgauntlet.html")
+    return render_template("runsscoredgauntlet.html")
 
 @app.route('/gauntlet', methods=['GET'])
 def runGauntlet():
-	print "hi there"
-	year = request.args.get('year', '')
-	if year == "":
-		return redirect(url_for('index'))
-	else:
-		for i in range(5):
-			t = ThreadUrl(queue, year)
-			t.setDaemon(True)
-			t.start()
-		for teamCode in teamCodes:
-			queue.put(teamCode)
-			print "\n " + "the length of the queue is " + queue.len
-		queue.join()
-		print "\n " + "k done now"
-		return render_template('show_results.html', rows=teams)
+    print "hi there"
+    year = request.args.get('year', '')
+    if year == "":
+        return redirect(url_for('index'))
+    else:
+        for i in range(5):
+            t = ThreadUrl(queue, year)
+            t.setDaemon(True)
+            t.start()
+        for teamCode in teamCodes:
+            queue.put(teamCode)
+            print "\n " + "the length of the queue is " + queue.len
+        queue.join()
+        print "\n " + "k done now"
+        return render_template('show_results.html', rows=teams)
